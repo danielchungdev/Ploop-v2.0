416038d5cdf9c4a3acde83d10347c94fbb78d72c
Pikachungg <pikachungg@gmail.com>
2021-10-24 04:38:36 -0400

    added basic functionality to login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './Sass/main.scss';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Login from './Pages/Login';
+import Home from './Pages/Home';
 
 function App() {
 	return (
 		<Router>
 			<Switch>
 				<Route exact path="/login" component={Login}></Route>
+				<Route exact path="/" component={Home}></Route>
 			</Switch>
 		</Router>
 	);
--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -8,19 +8,23 @@ export default function Login() {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
 
+    const login = () => {
+        alert("login has been clicked")
+    }
+
     return (
         <div className="login--container">
             <img src={Logo} alt="Ploop-logo"/>
             <div className="input--component">
                 <Typography variant="subtitle1">Username or Email</Typography>
-                <input type="text"/>
+                <input type="text" onChange={e=>setUsername(e.target.value)}/>
             </div>
             <div className="input--component">
                 <Typography variant="subtitle1">Password</Typography>
-                <input type="text"/>
+                <input type="password" onChange={e=>setPassword(e.target.value)}/>
             </div>
             <p className="links"><Link to="#">Forgot password?</Link></p>
-            <button className="normal--button">Log In</button>
+            <button className="normal--button" onClick={login}>Log In</button>
             <p className="links">Don't have an account? <Link to="#">Sign up</Link></p>
         </div>
     )
