5341a8b72eb8f5cd14b1f0bdf9d2ebdf40f5338c
landonbu <brandonbl2021@gmail.com>
2021-11-02 14:29:51 -0400

    single restroom

--- frontend/src/Pages/Restroom.js
+++ frontend/src/Pages/Restroom.js
@@ -10,7 +10,31 @@ export default function Restroom(){
     return(<div className = "restroom--container">
                 <img src={Logo} alt="Ploop-logo"/>
                 <img src={Default} alt="Default bathroom picture"/>
+
                 <Rating className = "restroom--rating" name="read-only" value={3} size = "large" readOnly/>
+                <div className="restroom--tag">
+                <p>Location: {}</p>
+                <div>
+                        <p>Cleanliness:</p>
+                        <Rating name="read-only" value = {3} readOnly size="small"/>
+                </div>
+                <div>
+                        <p>Ambiance:</p>
+                        <Rating name="read-only" value = {3} readOnly size="small"/>
+                </div>
+                <div>
+                        <p>Crowdedness:</p>
+                        <Rating name="read-only" value = {3} readOnly size="small"/>
+                </div>
+                <div>
+                        <p>Lighting:</p>
+                        <Rating name="read-only" value = {3} readOnly size="small"/>
+                </div>
+                <div>
+                        <p>Blowdrying</p>
+                </div>
+                <p>Baby Station: </p>
+</div>
 
         
         </div>)
